417149d9818dbf6b1690f28e5c44cce1ba3e35cf
whacko23 <Whacko23@users.noreply.github.com>
2022-04-11 23:04:38 -0500

    Added image assets and icons

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rajanish Karki on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
